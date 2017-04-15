5fab9fd73e40eec4f4981228042ce5366848a959
Shoma SATO <noir.neo.04@gmail.com>
2017-04-15 22:39:52 +0900

    :recycle: Gmail 関連の処理をクラス分割

--- lib/lita/handlers/kintai.rb
+++ lib/lita/handlers/kintai.rb
@@ -1,10 +1,4 @@
-require 'google/apis/gmail_v1'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
-
-require 'fileutils'
 require 'date'
-
 require 'rufus-scheduler'
 
 module Lita
@@ -21,14 +15,6 @@ module Lita
 
       on :loaded, :load_on_start
       on :slack_reaction_added, :reaction_added
-
-      OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
-      APPLICATION_NAME = 'Lita Kintai'
-      CLIENT_SECRETS_PATH = 'client_secret.json'
-      CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
-                                   "lita-kintai.yaml")
-      SCOPE = Google::Apis::GmailV1::AUTH_GMAIL_READONLY
-      USER_ID = 'default'
 
       def kintai(response)
         response.reply(current_kintai)
@@ -58,15 +44,14 @@ module Lita
 
       def code(response)
         code = response.matches[0][0]
-        authorizer.get_and_store_credentials_from_code(
-          user_id: USER_ID, code: code, base_url: OOB_URI)
+        Gmail.credentials_from_code
 
         response.reply("Confirmed")
       end
 
       def current_kintai
-        if authorize.nil?
-          auth_url = authorizer.get_authorization_url(base_url: OOB_URI)
+        if Gmail.authorize.nil?
+          auth_url = Gmail.authorization_url
           return <<-EOS
 Authenticate your Google account.
 Then tell me the code as follows: `code \#{your_code}`
@@ -82,7 +67,7 @@ Then tell me the code as follows: `code \#{your_code}`
         texts = ""
         texts << "#{Date.today.strftime("%m/%d")} (#{%w(日 月 火 水 木 金 土)[Date.today.wday]})#{config.template_header}"
 
-        mails = find_mail(config.query)
+        mails = Gmail.find_mail(config.query)
         # query の `newer:#{Date.today.strftime("%Y/%m/%d")}` 昨日のも一部返ってくる
         # `newer_than:1d` だと24h以内になるので、ここで今日のだけにする
         mails.select{ |m| m[:date] > Date.today.to_time }.each do |m|
@@ -117,58 +102,6 @@ Then tell me the code as follows: `code \#{your_code}`
         end
 
         texts << config.template_footer
-      end
-
-      def authorize
-        return @service if !@servise.nil? && !@servise.authorization.nil?
-
-        credentials = authorizer.get_credentials(USER_ID)
-        if credentials
-          @service.authorization = credentials
-          return @service
-        end
-
-        return nil
-      end
-
-      def authorizer
-        return @authorizer unless @authorizer.nil?
-
-        @service = Google::Apis::GmailV1::GmailService.new
-        @service.client_options.application_name = APPLICATION_NAME
-
-        FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
-
-        client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
-        token_store = Google::Auth::Stores::FileTokenStore.new(
-          file: CREDENTIALS_PATH)
-        @authorizer = Google::Auth::UserAuthorizer.new(
-          client_id, SCOPE, token_store)
-      end
-
-      def find_mail(query)
-        ids = @service.list_user_messages('me', q: query)
-
-        return [] unless ids.messages
-        ids.messages.map do |message|
-          find_mail_by_id(message.id)
-        end
-      end
-
-      def find_mail_by_id(id)
-        results = @service.get_user_message('me', id)
-
-        body = results.payload.parts ?
-          results.payload.parts.first.body.data :
-          results.payload.body.data
-        headers = results.payload.headers
-
-        {
-          subject: headers.select { |e| e.name == 'Subject'}.first.value,
-          from: headers.select { |e| e.name == 'From'}.first.value,
-          date: Time.parse(headers.select { |e| e.name == 'Date'}.first.value),
-          body: body.force_encoding('utf-8'),
-        }
       end
 
       Lita.register_handler(self)
